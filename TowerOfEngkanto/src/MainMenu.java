b692f1a9f0aff1c6d2426a8bf584d4efeeee2a22
Muwiii16 <gonzalesluigixd@gmail.com>
2026-05-11 14:57:55 +0800

    Created an empty window for main menu

--- TowerOfEngkanto/src/MainMenu.java
+++ TowerOfEngkanto/src/MainMenu.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.*;
+
+public class MainMenu extends JFrame {
+    public MainMenu() {
+        setTitle("Tower of Engkanto");
+        setSize(1000, 650);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainMenu menu = new MainMenu();
+            menu.setVisible(true);
+        });
     }
 }
